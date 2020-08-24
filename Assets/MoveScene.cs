234035a986051733e7c4370dd7375b93de5372cb
Nathan Kim <nathanjkim@uchicago.edu>
2020-08-24 02:18:01 -0400

    First Commit

---
+++ Assets/MoveScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MoveScene : MonoBehaviour
+{
+    [SerializeField] private string loadLevel;
+		void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+            SceneManager.LoadScene(loadLevel);
+            }
+        }
+	
+}
